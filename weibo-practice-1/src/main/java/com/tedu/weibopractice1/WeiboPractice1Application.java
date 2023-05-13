bfbf8e44781545fccef3797b134e20c657e03214
arcoshin <arcoshin@gmail.com>
2023-05-13 10:14:12 +0800

    practice

---
+++ weibo-practice-1/src/main/java/com/tedu/weibopractice1/WeiboPractice1Application.java
@@ -0,0 +1,13 @@
+package com.tedu.weibopractice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiboPractice1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeiboPractice1Application.class, args);
+    }
+
+}
---
+++ weibo-practice-1/src/test/java/com/tedu/weibopractice1/WeiboPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.weibopractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiboPractice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
